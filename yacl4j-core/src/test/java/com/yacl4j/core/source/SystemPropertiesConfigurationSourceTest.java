4e7e2d6e24be4f9826ba3c025fc1a61aed78d427
Fabrizio Cucci <fabrizio.cucci@gmail.com>
2016-08-28 12:54:38 +0200

    Refactored SystemPropertiesConfigurationSourceTest
    Now the test uses jMockit like the EnvironmentVariablesConfigurationSourceTest.

--- yacl4j-core/src/test/java/com/yacl4j/core/source/SystemPropertiesConfigurationSourceTest.java
+++ yacl4j-core/src/test/java/com/yacl4j/core/source/SystemPropertiesConfigurationSourceTest.java
@@ -5,100 +5,132 @@ import static org.junit.Assert.*;
 
 import java.util.Properties;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.yacl4j.core.util.ConfigurationUtils;
 import com.yacl4j.core.util.JsonPointerUtils;
 
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.integration.junit4.JMockit;
+
+@RunWith(JMockit.class)
 public class SystemPropertiesConfigurationSourceTest {
 
 	private final SystemPropertiesConfigurationSource systemPropertiesConfigurationSource = new SystemPropertiesConfigurationSource();
 	
-	private Properties systemProperties;
-	
-	@Before
-	public void cloneSystemProperties() {
-		this.systemProperties = (Properties) System.getProperties().clone();
-	}
-	
-	@After
-	public void restoreSystemProperties() {
-		System.setProperties(systemProperties);
-	}
-	
 	@Test
 	public void testSimpleSystemProperty() {
 		
-		String propertyKey = "property";
-		String propertyValue = "value";
-		System.setProperty(propertyKey, propertyValue);
+		String systemPropertyKey = "property";
+		String systemPropertyValue = "value";
+		
+		new MockUp<System>() {
+			@Mock
+	        Properties getProperties() {
+	        	Properties properties = new Properties();
+	        	properties.setProperty(systemPropertyKey, systemPropertyValue);
+	            return properties;
+	        }
+	    };
 		
 		JsonNode configuration = systemPropertiesConfigurationSource.getConfiguration();
 		
-		String actualPropertyValue = configuration.get(propertyKey).asText();
-		assertThat(actualPropertyValue, is(equalTo(propertyValue)));
+		String actualPropertyValue = configuration.get(systemPropertyKey).asText();
+		assertThat(actualPropertyValue, is(equalTo(systemPropertyValue)));
 	}
 	
 	@Test
 	public void testNestedSystemProperty() {
 		
-		String propertyKey = "/nested/property";
-		String propertyValue = "value";
-		System.setProperty(propertyKey, propertyValue);
+		String systemPropertyKey = "/nested/property";
+		String systemPropertyValue = "value";
+		
+		new MockUp<System>() {
+			@Mock
+	        Properties getProperties() {
+	        	Properties properties = new Properties();
+	        	properties.setProperty(systemPropertyKey, systemPropertyValue);
+	            return properties;
+	        }
+	    };
 		
 		JsonNode configuration = systemPropertiesConfigurationSource.getConfiguration();
 		
-		String actualPropertyValue = configuration.at(JsonPointerUtils.fromProperty(propertyKey)).asText();
-		assertThat(actualPropertyValue, is(equalTo(propertyValue)));
+		String actualPropertyValue = configuration.at(JsonPointerUtils.fromProperty(systemPropertyKey)).asText();
+		assertThat(actualPropertyValue, is(equalTo(systemPropertyValue)));
 	}
 	
 	@Test
 	public void testNestedSystemPropertiesWithCommonHead() {
 		
-		String property1Key = "/nested/property1";
-		String property1Value = "value1";
-		System.setProperty(property1Key, property1Value);
+		String systemProperty1Key = "/nested/property1";
+		String systemProperty1Value = "value1";
 		
-		String property2Key = "/nested/property2";
-		String property2Value = "value2";
-		System.setProperty(property2Key, property2Value);
+		String systemProperty2Key = "/nested/property2";
+		String systemProperty2Value = "value2";
+		
+		new MockUp<System>() {
+			@Mock
+	        Properties getProperties() {
+	        	Properties properties = new Properties();
+	        	properties.setProperty(systemProperty1Key, systemProperty1Value);
+	        	properties.setProperty(systemProperty2Key, systemProperty2Value);
+	            return properties;
+	        }
+	    };
 		
 		JsonNode configuration = systemPropertiesConfigurationSource.getConfiguration();
 		
-		String actualProperty1Value = configuration.at(JsonPointerUtils.fromProperty(property1Key)).asText();
-		assertThat(actualProperty1Value, is(equalTo(property1Value)));
+		String actualProperty1Value = configuration.at(JsonPointerUtils.fromProperty(systemProperty1Key)).asText();
+		assertThat(actualProperty1Value, is(equalTo(systemProperty1Value)));
 		
-		String actualProperty2Value = configuration.at(JsonPointerUtils.fromProperty(property2Key)).asText();
-		assertThat(actualProperty2Value, is(equalTo(property2Value)));
+		String actualProperty2Value = configuration.at(JsonPointerUtils.fromProperty(systemProperty2Key)).asText();
+		assertThat(actualProperty2Value, is(equalTo(systemProperty2Value)));
 	}
 	
 	@Test
 	public void testSystemPropertyWhenTheValueIsAnObject() {
 		
-		String propertyKey = "object";
-		JsonNode propertyValue = ConfigurationUtils.Yaml.fromString("key: value");
-		System.setProperty(propertyKey, propertyValue.toString());
+		String systemPropertyKey = "object";
+		JsonNode systemPropertyValue = ConfigurationUtils.Yaml.fromString("key: value");
+		
+		new MockUp<System>() {
+			@Mock
+	        Properties getProperties() {
+	        	Properties properties = new Properties();
+	        	properties.setProperty(systemPropertyKey, systemPropertyValue.toString());
+	            return properties;
+	        }
+	    };
 		
 		JsonNode configuration = systemPropertiesConfigurationSource.getConfiguration();
 		
-		JsonNode actualPropertyValue = configuration.at(JsonPointerUtils.fromProperty(propertyKey));
-		assertThat(actualPropertyValue, is(equalTo(propertyValue)));
+		JsonNode actualPropertyValue = configuration.at(JsonPointerUtils.fromProperty(systemPropertyKey));
+		assertThat(actualPropertyValue, is(equalTo(systemPropertyValue)));
 	}
 	
 	@Test
 	public void testSystemPropertyWhenTheValueIsAnArray() {
 		
-		String propertyKey = "array";
-		JsonNode propertyValue = ConfigurationUtils.Yaml.fromString("- element");
-		System.setProperty(propertyKey, propertyValue.toString());
+		String systemPropertyKey = "array";
+		JsonNode systemPropertyValue = ConfigurationUtils.Yaml.fromString("- element");
+		
+		new MockUp<System>() {
+			@Mock
+	        Properties getProperties() {
+	        	Properties properties = new Properties();
+	        	properties.setProperty(systemPropertyKey, systemPropertyValue.toString());
+	            return properties;
+	        }
+	    };
 		
 		JsonNode configuration = systemPropertiesConfigurationSource.getConfiguration();
 		
-		JsonNode actualPropertyValue = configuration.at(JsonPointerUtils.fromProperty(propertyKey));
-		assertThat(actualPropertyValue, is(equalTo(propertyValue)));
+		JsonNode actualPropertyValue = configuration.at(JsonPointerUtils.fromProperty(systemPropertyKey));
+		assertThat(actualPropertyValue, is(equalTo(systemPropertyValue)));
 	}
 	
 }
