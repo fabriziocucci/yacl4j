ec47f76ea45575919c7966032d62fc914972d8b6
Fabrizio Cucci <fabrizio.cucci@gmail.com>
2016-09-28 18:02:13 +0100

    Refactoring ConfigurationSourceBuilder class
    Moved file specific logic from the ConfigurationSourceBuilder class to the FileConfigurationSource class.

--- yacl4j-core/src/main/java/com/yacl4j/core/source/ConfigurationSourceBuilder.java
+++ yacl4j-core/src/main/java/com/yacl4j/core/source/ConfigurationSourceBuilder.java
@@ -1,12 +1,8 @@
 package com.yacl4j.core.source;
 
 import java.io.File;
-import java.net.URL;
 
 import com.yacl4j.core.ConfigurationBuilder;
-import com.yacl4j.core.ConfigurationSource;
-import com.yacl4j.core.util.ConfigurationUtils;
-import com.yacl4j.core.util.FileUtils;
 
 public class ConfigurationSourceBuilder {
 	
@@ -17,25 +13,15 @@ public class ConfigurationSourceBuilder {
 	}
 
 	public ConfigurationBuilder file(File file) {
-		return this.configurationBuilder.source(selectFileConfigurationSource(file));
+		return this.configurationBuilder.source(FileConfigurationSource.fromFile(file));
 	}
 	
 	public ConfigurationBuilder fileFromClasspath(String filename) {
-		URL configurationUrl = this.getClass().getClassLoader().getResource(filename);
-		if (configurationUrl != null) {
-			return this.configurationBuilder.source(selectFileConfigurationSource(FileUtils.toFile(configurationUrl)));
-		} else {
-			throw new IllegalStateException("Unable to find file on classpath: " + filename);
-		}
+		return this.configurationBuilder.source(FileConfigurationSource.fromFileOnClasspath(filename));
 	}
 	
 	public ConfigurationBuilder fileFromPath(String filename) {
-		File configurationFile = new File(filename);
-		if (configurationFile.exists()) {
-			return this.configurationBuilder.source(selectFileConfigurationSource(configurationFile));
-		} else {
-			throw new IllegalStateException("Unable to find file on path: " + filename);
-		}
+		return this.configurationBuilder.source(FileConfigurationSource.fromFileOnPath(filename));
 	}
 	
 	public ConfigurationBuilder systemProperties() {
@@ -44,18 +30,6 @@ public class ConfigurationSourceBuilder {
 	
 	public ConfigurationBuilder environmentVariables() {
 		return this.configurationBuilder.source(new EnvironmentVariablesConfigurationSource());
-	}
-	
-	private ConfigurationSource selectFileConfigurationSource(File file) {
-		if (file.getName().endsWith(".properties")) {
-			return new FileConfigurationSource(file, ConfigurationUtils.Properties::fromFile);
-		} else if (file.getName().endsWith(".yaml")) {
-			return new FileConfigurationSource(file, ConfigurationUtils.Yaml::fromFile);
-		} else if (file.getName().endsWith(".json")) {
-			return new FileConfigurationSource(file, ConfigurationUtils.Json::fromFile);
-		} else {
-			throw new IllegalStateException("Configuration format not supported: " + file);
-		}
 	}
 	
 }
--- yacl4j-core/src/main/java/com/yacl4j/core/source/FileConfigurationSource.java
+++ yacl4j-core/src/main/java/com/yacl4j/core/source/FileConfigurationSource.java
@@ -1,10 +1,13 @@
 package com.yacl4j.core.source;
 
 import java.io.File;
+import java.net.URL;
 import java.util.function.Function;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.yacl4j.core.ConfigurationSource;
+import com.yacl4j.core.util.ConfigurationUtils;
+import com.yacl4j.core.util.FileUtils;
 
 class FileConfigurationSource implements ConfigurationSource {
 
@@ -19,6 +22,40 @@ class FileConfigurationSource implements ConfigurationSource {
 	@Override
 	public JsonNode getConfiguration() {
 		return configurationFileReader.apply(configurationFile);
+	}
+	
+	static FileConfigurationSource fromFileOnClasspath(String filename) {
+		URL configurationUrl = FileConfigurationSource.class.getClassLoader().getResource(filename);
+		if (configurationUrl != null) {
+			return selectFileConfigurationSource(FileUtils.toFile(configurationUrl));
+		} else {
+			throw new IllegalStateException("Unable to find file on classpath: " + filename);
+		}
+	}
+	
+	static FileConfigurationSource fromFileOnPath(String filename) {
+		File configurationFile = new File(filename);
+		if (configurationFile.exists()) {
+			return selectFileConfigurationSource(configurationFile);
+		} else {
+			throw new IllegalStateException("Unable to find file on path: " + filename);
+		}
+	}
+	
+	static FileConfigurationSource fromFile(File file) {
+		return selectFileConfigurationSource(file);
+	}
+	
+	private static FileConfigurationSource selectFileConfigurationSource(File file) {
+		if (file.getName().endsWith(".properties")) {
+			return new FileConfigurationSource(file, ConfigurationUtils.Properties::fromFile);
+		} else if (file.getName().endsWith(".yaml")) {
+			return new FileConfigurationSource(file, ConfigurationUtils.Yaml::fromFile);
+		} else if (file.getName().endsWith(".json")) {
+			return new FileConfigurationSource(file, ConfigurationUtils.Json::fromFile);
+		} else {
+			throw new IllegalStateException("Configuration format not supported: " + file);
+		}
 	}
 	
 }
