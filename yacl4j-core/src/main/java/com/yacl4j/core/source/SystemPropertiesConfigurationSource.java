bd4d0922d9734fd5d0981a3cd4c22d35bed2330b
Fabrizio Cucci <fabrizio.cucci@gmail.com>
2016-10-09 10:40:31 +0100

    No more clone of system properties before transformation
    System properties were cloned before being transformed into a JsonNode configuration. We are no longer cloning them because there is no real advantage in doing it. Moreover, we have aligned the behavior to the EnvironmentVariablesConfigurationSource class.
    
    It would probably make sense to clone system properties and environment variables in the constructor of the respective sources because that would guarantee consistent results over multiple invocation of the ConfigurationBuilder#build method. But again, that's not always the desired behavior so, in the worst case, we should make this configurable.

--- yacl4j-core/src/main/java/com/yacl4j/core/source/SystemPropertiesConfigurationSource.java
+++ yacl4j-core/src/main/java/com/yacl4j/core/source/SystemPropertiesConfigurationSource.java
@@ -1,7 +1,5 @@
 package com.yacl4j.core.source;
 
-import java.util.Properties;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.yacl4j.core.ConfigurationSource;
 import com.yacl4j.core.util.ConfigurationUtils;
@@ -10,7 +8,7 @@ class SystemPropertiesConfigurationSource implements ConfigurationSource {
 
 	@Override
 	public JsonNode getConfiguration() {
-		return ConfigurationUtils.Properties.fromProperties((Properties) System.getProperties().clone());
+		return ConfigurationUtils.Properties.fromProperties(System.getProperties());
 	}
 	
 }
